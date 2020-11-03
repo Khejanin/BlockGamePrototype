c3160a2d9095caf3c8ad5d69e564130e887d682a
dmar98 <dmar@outlook.de>
2020-11-03 13:34:52 +0100

    Added Ui Elements

--- GDGame/Game/Scenes/MainScene.cs
+++ GDGame/Game/Scenes/MainScene.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using GDGame.Game.Controllers;
 using GDGame.Game.Controllers.CameraControllers;
 using GDGame.Game.Factory;
+using GDGame.Game.UI;
 using GDGame.Game.Utilities;
 using GDLibrary;
 using GDLibrary.Actors;
@@ -23,20 +24,20 @@ namespace GDGame.Scenes
         private VertexPositionColorTexture[] vertices;
         private Texture2D grass;
         private PrimitiveObject archetypalTexturedQuad;
-        
+
         private SoundEffect track01, track02, track03, track04, track05;
-        
+
         private Texture2D backSky;
         private Texture2D leftSky;
         private Texture2D rightSky;
         private Texture2D frontSky;
         private Texture2D topSky;
 
-        
+
         public MainScene(Main game) : base(game)
         {
         }
-        
+
         public override void Initialize()
         {
             InitCameras3D();
@@ -44,8 +45,9 @@ namespace GDGame.Scenes
             InitDrawnContent();
             InitSound();
         }
-        
-         #region Initialization - Vertices, Archetypes, Helpers, Drawn Content(e.g. Skybox)
+
+        #region Initialization - Vertices, Archetypes, Helpers, Drawn Content(e.g. Skybox)
+
         private void InitDrawnContent() //formerly InitPrimitives
         {
             //add archetypes that can be cloned
@@ -65,8 +67,11 @@ namespace GDGame.Scenes
 
             //grid
             InitGrid();
+
+            InitHud();
         }
-        
+
+
         private void InitTextures()
         {
             //step 1 - texture
@@ -80,34 +85,37 @@ namespace GDGame.Scenes
                 = Content.Load<Texture2D>("Assets/Textures/Skybox/front");
             topSky
                 = Content.Load<Texture2D>("Assets/Textures/Skybox/sky");
-
             grass
                 = Content.Load<Texture2D>("Assets/Textures/Foliage/Ground/grass1");
         }
-        
+
         private void InitCameras3D()
         {
-            Transform3D transform3D = new Transform3D(new Vector3(10,10,20), -Vector3.Forward,Vector3.Up);
+            Transform3D transform3D = new Transform3D(new Vector3(10, 10, 20), -Vector3.Forward, Vector3.Up);
             Camera3D camera3D = null;
-            
-            camera3D = new Camera3D("camcam",ActorType.Camera3D,StatusType.Update,transform3D,ProjectionParameters.StandardDeepSixteenTen);
 
-            camera3D.ControllerList.Add(new RotationAroundActor("main cam",ControllerType.FlightCamera, KeyboardManager,1));
-            
+            camera3D = new Camera3D("camcam", ActorType.Camera3D, StatusType.Update, transform3D,
+                ProjectionParameters.StandardDeepSixteenTen);
+
+            camera3D.ControllerList.Add(new RotationAroundActor("main cam", ControllerType.FlightCamera,
+                KeyboardManager, 1));
+
             CameraManager.Add(camera3D);
-            
+
             CameraManager.ActiveCameraIndex = 0; //0, 1, 2, 3
         }
 
         private void InitGrid()
         {
-            Grid grid = new Grid(new TileFactory(game.KeyboardManager, game.ObjectManager, game.Content, game.ModelEffect));
+            Grid grid = new Grid(new TileFactory(game.KeyboardManager, game.ObjectManager, game.Content,
+                game.ModelEffect));
             grid.GenerateGrid(@"Game\LevelFiles\AttachTest.json");
 
             List<DrawnActor3D> players = ObjectManager.FindAll(actor3D => actor3D.ActorType == ActorType.Player);
             if (players.Count > 0)
             {
-                RotationAroundActor rotationAroundActor = (RotationAroundActor) CameraManager.ActiveCamera.ControllerList[0];
+                RotationAroundActor rotationAroundActor =
+                    (RotationAroundActor) CameraManager.ActiveCamera.ControllerList[0];
                 rotationAroundActor.Target = players[0];
             }
         }
@@ -116,10 +124,10 @@ namespace GDGame.Scenes
         {
             //transform
             Transform3D transform3D = new Transform3D(Vector3.Up,
-                                Vector3.Zero,       //rotation
-                                Vector3.One,        //scale
-                                    -Vector3.UnitZ,         //look
-                                    Vector3.UnitY);         //up
+                Vector3.Zero, //rotation
+                Vector3.One, //scale
+                -Vector3.UnitZ, //look
+                Vector3.UnitY); //up
 
             //effectparameters
             EffectParameters effectParameters = new EffectParameters(game.ModelEffect,
@@ -134,48 +142,22 @@ namespace GDGame.Scenes
                 StatusType.Drawn | StatusType.Update, transform3D,
                 effectParameters, model);
             this.objectManager.Add(archetypalBoxObject);*/
-            
+
             EffectParameters wireframeEffectParameters =
                 new EffectParameters(game.ModelEffect, null, Color.White, 1);
-            
-            
+
+
             archetypalBoxWireframe = new ModelObject("original wireframe box mesh",
-            ActorType.Helper, StatusType.Update | StatusType.Drawn , transform3D, wireframeEffectParameters,model,game.WireframeRasterizerState);
+                ActorType.Helper, StatusType.Update | StatusType.Drawn, transform3D, wireframeEffectParameters, model,
+                game.WireframeRasterizerState);
 
             ObjectManager.Add(archetypalBoxWireframe);
-        }
-
-        private void InitVertices()
-        {
-            vertices
-                = new VertexPositionColorTexture[4];
-
-            float halfLength = 0.5f;
-            //TL
-            vertices[0] = new VertexPositionColorTexture(
-                new Vector3(-halfLength, halfLength, 0),
-                new Color(255, 255, 255, 255), new Vector2(0, 0));
-
-            //BL
-            vertices[1] = new VertexPositionColorTexture(
-                new Vector3(-halfLength, -halfLength, 0),
-                Color.White, new Vector2(0, 1));
-
-            //TR
-            vertices[2] = new VertexPositionColorTexture(
-                new Vector3(halfLength, halfLength, 0),
-                Color.White, new Vector2(1, 0));
-
-            //BR
-            vertices[3] = new VertexPositionColorTexture(
-                new Vector3(halfLength, -halfLength, 0),
-                Color.White, new Vector2(1, 1));
         }
 
         private void InitPrimitiveArchetypes() //formerly InitTexturedQuad
         {
             Transform3D transform3D = new Transform3D(Vector3.Zero, Vector3.Zero,
-               Vector3.One, Vector3.UnitZ, Vector3.UnitY);
+                Vector3.One, Vector3.UnitZ, Vector3.UnitY);
 
             EffectParameters effectParameters = new EffectParameters(game.UnlitTexturedEffect,
                 grass, /*bug*/ Color.White, 1);
@@ -199,11 +181,11 @@ namespace GDGame.Scenes
 
             //step 1 - vertices
             VertexPositionColor[] vertices = VertexFactory.GetVerticesPositionColorOriginHelper(
-                                    out primitiveType, out primitiveCount);
+                out primitiveType, out primitiveCount);
 
             //step 2 - make vertex data that provides Draw()
-            IVertexData vertexData = new VertexData<VertexPositionColor>(vertices, 
-                                    primitiveType, primitiveCount);
+            IVertexData vertexData = new VertexData<VertexPositionColor>(vertices,
+                primitiveType, primitiveCount);
 
             //step 3 - make the primitive object
             Transform3D transform3D = new Transform3D(new Vector3(0, 20, 0),
@@ -218,71 +200,8 @@ namespace GDGame.Scenes
                 ActorType.Helper, StatusType.Drawn, transform3D, effectParameters, vertexData);
 
             ObjectManager.Add(primitiveObject);
-
         }
 
-        //private void InitSkybox()
-        //{ 
-        //    //back
-        //    primitiveObject = this.archetypalTexturedQuad.Clone() as PrimitiveObject;
-        //  //  primitiveObject.StatusType = StatusType.Off; //Experiment of the effect of StatusType
-        //    primitiveObject.ID = "sky back";
-        //    primitiveObject.EffectParameters.Texture = this.backSky;
-        //    primitiveObject.Transform3D.Scale = new Vector3(worldScale, worldScale, 1);
-        //    primitiveObject.Transform3D.Translation = new Vector3(0, 0, -worldScale / 2.0f);
-        //    this.objectManager.Add(primitiveObject);
-
-        //    //left
-        //    primitiveObject = this.archetypalTexturedQuad.Clone() as PrimitiveObject;
-        //    primitiveObject.ID = "left back";
-        //    primitiveObject.EffectParameters.Texture = this.leftSky;
-        //    primitiveObject.Transform3D.Scale = new Vector3(worldScale, worldScale, 1);
-        //    primitiveObject.Transform3D.RotationInDegrees = new Vector3(0, 90, 0);
-        //    primitiveObject.Transform3D.Translation = new Vector3(-worldScale / 2.0f, 0, 0);
-        //    this.objectManager.Add(primitiveObject);
-
-        //    //right
-        //    primitiveObject = this.archetypalTexturedQuad.Clone() as PrimitiveObject;
-        //    primitiveObject.ID = "sky right";
-        //    primitiveObject.EffectParameters.Texture = this.rightSky;
-        //    primitiveObject.Transform3D.Scale = new Vector3(worldScale, worldScale, 20);
-        //    primitiveObject.Transform3D.RotationInDegrees = new Vector3(0, -90, 0);
-        //    primitiveObject.Transform3D.Translation = new Vector3(worldScale / 2.0f, 0, 0);
-        //    this.objectManager.Add(primitiveObject);
-
-             
-        //    //top
-        //    primitiveObject = this.archetypalTexturedQuad.Clone() as PrimitiveObject;
-        //    primitiveObject.ID = "sky top";
-        //    primitiveObject.EffectParameters.Texture = this.topSky;
-        //    primitiveObject.Transform3D.Scale = new Vector3(worldScale, worldScale, 1);
-        //    primitiveObject.Transform3D.RotationInDegrees = new Vector3(90, -90, 0);
-        //    primitiveObject.Transform3D.Translation = new Vector3(0 ,worldScale / 2.0f, 0);
-        //    this.objectManager.Add(primitiveObject);
-
-        //    //to do...front
-        //    primitiveObject = this.archetypalTexturedQuad.Clone() as PrimitiveObject;
-        //    primitiveObject.ID = "sky front";
-        //    primitiveObject.EffectParameters.Texture = this.frontSky;
-        //    primitiveObject.Transform3D.Scale = new Vector3(worldScale, worldScale, 1);
-        //    primitiveObject.Transform3D.RotationInDegrees = new Vector3(0, 180, 0);
-        //    primitiveObject.Transform3D.Translation = new Vector3(0, 0, worldScale / 2.0f);
-        //    this.objectManager.Add(primitiveObject);
-
-        //}
-
-        //private void InitGround()
-        //{
-        //    //grass
-        //    primitiveObject = this.archetypalTexturedQuad.Clone() as PrimitiveObject;
-        //    primitiveObject.ID = "grass";
-        //    primitiveObject.EffectParameters.Texture = this.grass;
-        //    primitiveObject.Transform3D.Scale = new Vector3(worldScale, worldScale, 1);
-        //    primitiveObject.Transform3D.RotationInDegrees = new Vector3(90, 90, 0);
-        //    this.objectManager.Add(primitiveObject);
-        //}
-
-        
         private void InitSound()
         {
             //step 1 - load songs
@@ -301,8 +220,7 @@ namespace GDGame.Scenes
 
             SoundManager.playSoundEffect("main");
         }
-        
-        
+
         #endregion
 
         public override void Update(GameTime gameTime)
@@ -312,7 +230,7 @@ namespace GDGame.Scenes
                 CameraManager.CycleActiveCamera();
                 // this.cameraManager.ActiveCameraIndex++;
             }
-            
+
             if (KeyboardManager.IsFirstKeyPress(Keys.C))
             {
                 CameraManager.CycleActiveCamera();
@@ -332,17 +250,17 @@ namespace GDGame.Scenes
         public override void Draw(GameTime gameTime)
         {
         }
-        
-         private void RaycastTests()
+
+        private void RaycastTests()
         {
             if (KeyboardManager.IsFirstKeyPress(Keys.G))
             {
-                ModelObject o = (ModelObject)archetypalBoxWireframe.Clone();
-                o.ControllerList.Add(new CustomBoxColliderController(ColliderType.Cube,1));
+                ModelObject o = (ModelObject) archetypalBoxWireframe.Clone();
+                o.ControllerList.Add(new CustomBoxColliderController(ColliderType.Cube, 1));
                 o.Transform3D = new Transform3D(Vector3.Up * 5, -Vector3.Forward, Vector3.Up);
                 ObjectManager.Add(o);
 
-                o = (ModelObject)o.Clone();
+                o = (ModelObject) o.Clone();
                 o.Transform3D.Translation = new Vector3(5, 5, 0);
                 ObjectManager.Add(o);
             }
@@ -351,40 +269,46 @@ namespace GDGame.Scenes
             {
                 List<Raycaster.HitResult> hit = Raycaster.RaycastAll(new Vector3(0, 5, -5), new Vector3(0, 0, 1),
                     ObjectManager.FindAll(a => a != null));
-                   
+
                 Debug.WriteLine("NEW HIT : MULTI");
-                   
+
                 Debug.WriteLine("List size : " + hit.Count);
-                   
+
                 foreach (Raycaster.HitResult result in hit)
                 {
                     Debug.WriteLine("DISTANCE : " + result.distance + " ,ACTOR:" + result.actor);
                 }
-                
-                hit.Sort((result, hitResult) => (int)(result.distance - hitResult.distance));
-                   
+
+                hit.Sort((result, hitResult) => (int) (result.distance - hitResult.distance));
+
                 hit = Raycaster.RaycastAll(new Vector3(-5, 5, 0), new Vector3(1, 0, 0),
                     ObjectManager.FindAll(a => a != null));
-                   
+
                 Debug.WriteLine("NEW HIT : MULTI");
-                   
+
                 Debug.WriteLine("List size : " + hit.Count);
-                
-                hit.Sort((result, hitResult) => (int)(result.distance - hitResult.distance));
-                
+
+                hit.Sort((result, hitResult) => (int) (result.distance - hitResult.distance));
+
                 foreach (Raycaster.HitResult result in hit)
                 {
                     Debug.WriteLine("DISTANCE : " + result.distance + " ,ACTOR:" + result.actor);
                 }
-                
+
                 Debug.WriteLine("NEW HIT : SINGLE");
-                
+
                 Raycaster.HitResult hitSingle = Raycaster.Raycast(new Vector3(-5, 5, 0), new Vector3(1, 0, 0),
                     ObjectManager.FindAll(a => a != null));
-                
+
                 Debug.WriteLine("DISTANCE : " + hitSingle.distance + " ,ACTOR:" + hitSingle.actor);
             }
         }
-         
+
+        private void InitHud()
+        {
+            HUD hud = new HUD(game, Content.Load<Texture2D>("Assets/Textures/Base/WhiteSquare"),
+                Content.Load<SpriteFont>("Assets/Fonts/Arial"), new SpriteBatch(GraphicsDevice));
+            game.Components.Add(hud);
+        }
     }
 }
---
+++ GDGame/Game/UI/HUD.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GDGame.Game.UI
+{
+    public class HUD : DrawableGameComponent
+    {
+        private SpriteBatch spriteBatch;
+        private Texture2D texture2D;
+        private SpriteFont spriteFont;
+        private BlendState blendState;
+
+        public HUD(Microsoft.Xna.Framework.Game game, Texture2D texture2D, SpriteFont spriteFont,
+            SpriteBatch spriteBatch) : base(game)
+        {
+            this.texture2D = texture2D;
+            this.spriteFont = spriteFont;
+            this.spriteBatch = spriteBatch;
+
+            blendState = new BlendState();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin(SpriteSortMode.BackToFront, blendState);
+
+            int height = 40;
+            Point location = new Point(0, GameConstants.screenHeight - height);
+            Point size = new Point(GameConstants.screenWidth, height);
+
+            DrawTexture(location, size);
+
+            height = 90;
+            int width = 100;
+            int screenWidth = (GameConstants.screenWidth - width) / 2;
+            location = new Point(screenWidth, GameConstants.screenHeight - height);
+            size = new Point(width, height);
+
+            DrawTexture(location, size);
+
+            //spriteBatch.DrawString(spriteFont, "Test Level", Vector2.Zero, Color.Black);
+
+            StringBuilder stringBuilder = new StringBuilder("Moves");
+            Vector2 position = new Vector2(screenWidth, GameConstants.screenHeight - height);
+            spriteBatch.DrawString(spriteFont, stringBuilder, position, Color.Red);
+
+            stringBuilder = new StringBuilder("5");
+            position = new Vector2(screenWidth + 45, GameConstants.screenHeight - height + 26);
+            spriteBatch.DrawString(spriteFont, stringBuilder, position, Color.Red);
+            
+            stringBuilder = new StringBuilder("Current Level");
+            position = new Vector2(x: screenWidth / 4f, GameConstants.screenHeight - height / 2);
+            spriteBatch.DrawString(spriteFont, stringBuilder, position, Color.Red);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private void DrawTexture(Point location, Point size)
+        {
+            Rectangle rectangle = new Rectangle(location, size);
+            spriteBatch.Draw(texture2D, rectangle, Color.White);
+        }
+    }
+}
