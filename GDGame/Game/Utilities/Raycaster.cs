c36f2174bedb4b379dd0bfb9dadeeafc1b139cb2
Khejanin <khejanin@gmail.com>
2020-11-04 20:41:52 +0100

    I forgot something whoops

--- GDGame/Game/Utilities/Raycaster.cs
+++ GDGame/Game/Utilities/Raycaster.cs
@@ -11,80 +11,7 @@ namespace GDGame.Game.Utilities
 {
     public static class Raycaster
     {
-        public class HitResult : IComparable<HitResult>
-        {
-            public float distance;
-            public Vector3 hitPosition;
-            public Actor3D actor;
-
-            public int CompareTo(HitResult other)
-            {
-                return distance.CompareTo(other.distance);
-            }
-        }
-        
-        #region Pass by Reference Definitions
-
-        public static void Raycast(Vector3 position, Vector3 direction, ref List<HitResult> hit, float maxDist = float.MaxValue, List<Actor3D> ignoreList = null)
-        {
-            if(maxDist <= 0) throw new ArgumentException("You can't set a max cast distance to zero or negative!");
-            
-            List<DrawnActor3D> AllObjects = ObjectManager.GetAllObjects();
-            if(ignoreList != null) AllObjects.RemoveAll(actor3D => ignoreList.Contains(actor3D));
-            Ray ray = new Ray(position, direction);
-            foreach (DrawnActor3D drawnActor3D in AllObjects)
-            {
-                float? dist = -1f;
-
-                if(drawnActor3D is PrimitiveObject)
-                {
-                    PrimitiveColliderController pcc = drawnActor3D.ControllerList.Find(c => c is PrimitiveColliderController) 
-                        as PrimitiveColliderController;
-                    CustomBoxColliderController cbcc = null;
-                    if(pcc == null)  
-                        cbcc = drawnActor3D.ControllerList.Find(c => c is CustomBoxColliderController) 
-                            as CustomBoxColliderController;
-
-                    bool pccCheck = pcc != null && (dist = ray.Intersects(pcc.GetBounds())) != null;
-                    bool cbccCheck = cbcc != null && (dist = ray.Intersects(cbcc.GetBounds())) != null;
-                    
-                    if ((pccCheck || cbccCheck) && dist < maxDist)
-                    {
-                        HitResult result = new HitResult();
-                        result.actor = drawnActor3D;
-                        result.distance = (float)dist;
-                        result.hitPosition = position + direction * result.distance;
-                        hit.Add(result);
-                    }
-                }
-            }
-        }
-
-        #endregion
-        
-        #region Normal Definitions
-
-        public static HitResult Raycast(Vector3 position, Vector3 direction,float maxDist = float.MaxValue,List<Actor3D> ignoreList = null)
-        {
-            List<HitResult> all = RaycastAll(position, direction, maxDist,ignoreList);
-            all.Sort();
-
-            if (all.Count == 0)
-                return null;
-
-            return all[0];
-        }
-        
-        public static List<HitResult> RaycastAll(Vector3 position, Vector3 direction,float maxDist = float.MaxValue,List<Actor3D> ignoreList = null)
-        {
-            List<HitResult> result = new List<HitResult>();
-            Raycast(position, direction, ref result,maxDist,ignoreList);
-            return result;
-        }
-
-        #endregion
-
-        #region Actor Specific Definitions
+        #region Public Methods
 
         public static List<HitResult> PlayerCastAll(this CubePlayer player,List<Vector3> initialPositions, List<Vector3> endPositions)
         {
@@ -104,6 +31,51 @@ namespace GDGame.Game.Utilities
             return hit;
         }
 
+        public static void Raycast(Vector3 position, Vector3 direction, ref List<HitResult> hit, float maxDist = float.MaxValue, List<Actor3D> ignoreList = null)
+        {
+            if(maxDist <= 0) throw new ArgumentException("You can't set a max cast distance to zero or negative!");
+            
+            List<DrawnActor3D> AllObjects = ObjectManager.GetAllObjects();
+            if(ignoreList != null) AllObjects.RemoveAll(actor3D => ignoreList.Contains(actor3D));
+            Ray ray = new Ray(position, direction);
+            foreach (DrawnActor3D drawnActor3D in AllObjects)
+            {
+                float? dist = -1f;
+
+                PrimitiveColliderController pcc =
+                    drawnActor3D.ControllerList.Find(c => c is PrimitiveColliderController)
+                        as PrimitiveColliderController;
+                    
+                CustomBoxColliderController cbcc = null;
+                    if(pcc == null)
+                        cbcc = drawnActor3D.ControllerList.Find(c => c is CustomBoxColliderController) 
+                            as CustomBoxColliderController;
+
+                bool pccCheck = pcc != null && (dist = ray.Intersects(pcc.GetBounds())) != null; 
+                bool cbccCheck = cbcc != null && (dist = ray.Intersects(cbcc.GetBounds())) != null;
+                    
+                if ((pccCheck || cbccCheck) && dist < maxDist)
+                { 
+                    HitResult result = new HitResult(); 
+                    result.actor = drawnActor3D; 
+                    result.distance = (float)dist; 
+                    result.hitPosition = position + direction * result.distance; 
+                    hit.Add(result);
+                }
+            }
+        }
+
+        public static HitResult Raycast(Vector3 position, Vector3 direction,float maxDist = float.MaxValue,List<Actor3D> ignoreList = null)
+        {
+            List<HitResult> all = RaycastAll(position, direction, maxDist,ignoreList);
+            all.Sort();
+
+            if (all.Count == 0)
+                return null;
+
+            return all[0];
+        }
+
         public static HitResult Raycast(this DrawnActor3D callingDrawnActor3D, Vector3 position, Vector3 direction, bool ignoreSelf,float maxDist = Single.MaxValue)
         {
             List<HitResult> all = RaycastAll(callingDrawnActor3D,position, direction,ignoreSelf,maxDist);
@@ -115,8 +87,14 @@ namespace GDGame.Game.Utilities
             return all[0];
         }
 
-        
-        
+        public static List<HitResult> RaycastAll(Vector3 position, Vector3 direction,float maxDist = float.MaxValue,List<Actor3D> ignoreList = null)
+        {
+            List<HitResult> result = new List<HitResult>();
+            Raycast(position, direction, ref result,maxDist,ignoreList);
+            return result;
+        }
+
+
         public static List<HitResult> RaycastAll(this DrawnActor3D callingDrawnActor3D,Vector3 position, Vector3 direction, bool ignoreSelf,float maxDist = Single.MaxValue)
         {
             List<Actor3D> ignoreList = new List<Actor3D>();
@@ -127,5 +105,24 @@ namespace GDGame.Game.Utilities
 
         #endregion
 
+        public class HitResult : IComparable<HitResult>
+        {
+            #region Public variables
+
+            public Actor3D actor;
+            public float distance;
+            public Vector3 hitPosition;
+
+            #endregion
+
+            #region Public Methods
+
+            public int CompareTo(HitResult other)
+            {
+                return distance.CompareTo(other.distance);
+            }
+
+            #endregion
+        }
     }
 }
