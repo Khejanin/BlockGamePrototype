076cbce348fbba2d215f474dd89bdc6b599f9958
PaulBichler <paul.bichler@btsgp.lu>
2020-11-15 10:58:34 +0100

    Changed enemy tile to inherit from BasicTile

--- GDGame/Game/Actors/Tiles/EnemyTile.cs
+++ GDGame/Game/Actors/Tiles/EnemyTile.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 
 namespace GDGame.Game.Actors.Tiles
 {
-    class EnemyTile : MovableTile
+    class EnemyTile : BasicTile
     {
         private MovementComponent movementComponent;
 
