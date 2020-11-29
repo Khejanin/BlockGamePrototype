1c7bc82cb44f41178ce302edc6081829bc26e235
SimmoneMC <D00219638@student.dkit.ie>
2020-11-29 15:07:38 +0000

    new updated platform
    Takes in value and direction, and works out where to go based on that

--- GDGame/Game/Actors/Tiles/MovingPlatformTile.cs
+++ GDGame/Game/Actors/Tiles/MovingPlatformTile.cs
@@ -18,11 +18,21 @@ namespace GDGame.Actors
         private Vector3 currentPos;
         private Vector3 starPos;
         private int maxMove;
+        private int dir;
 
-        public MovingPlatformTile(string id, ActorType actorType, StatusType statusType, Transform3D transform, EffectParameters effectParameters, Model model, ETileType tileType, Vector3 endpos) : base(id, actorType, statusType, transform, effectParameters, model, tileType)
+        //-1 = X, 1 = Y, 0 = Z
+
+        public MovingPlatformTile(string id, ActorType actorType, StatusType statusType, Transform3D transform, EffectParameters effectParameters, Model model, ETileType tileType, int tileMoves, int dir) : base(id, actorType, statusType, transform, effectParameters, model, tileType)
         {
-            this.starPos = this.currentPos = this.Transform3D.Translation;
-            this.endPos = endpos;
+            this.starPos = this.currentPos = this.Transform3D.Translation; 
+            this.dir = dir;
+
+            if(dir == -1)
+                this.endPos = new Vector3((this.starPos.X + maxMove), this.starPos.Y, this.StartPos.Z);
+            else if(dir == 1)
+                this.endPos = new Vector3(this.starPos.X, (this.starPos.Y + maxMove), this.StartPos.Z);
+            else
+                this.endPos = new Vector3(this.starPos.X, this.starPos.Y, (this.starPos.Z + maxMove));
         }
 
         public override void InitializeTile()
@@ -58,7 +68,7 @@ namespace GDGame.Actors
         {
             MovingPlatformTile platform = new MovingPlatformTile("clone - " + ID, ActorType, StatusType,
                 Transform3D.Clone() as Transform3D,
-                EffectParameters.Clone() as EffectParameters, Model, TileType, endPos);
+                EffectParameters.Clone() as EffectParameters, Model, TileType, maxMove, dir);
 
             if (ControllerList != null)
             {
@@ -89,30 +99,43 @@ namespace GDGame.Actors
         public void movePlatform()
         {
             bool max = false;
+            int move = 0;
+            Vector3 b = new Vector3(0, 0, 0);
 
-            if (this.currentPos.X <= this.starPos.X)
-            {
+            if (this.currentPos.X <= this.starPos.X || this.currentPos.Y <= this.starPos.Y || this.currentPos.Z <= this.starPos.Z)
                 max = false;
-            }
 
-            if (this.currentPos.X >= this.endPos.X)
-            {
+            if (this.currentPos.X >= this.endPos.X || this.currentPos.Y >= this.starPos.Y || this.currentPos.Z >= this.starPos.Z)
                 max = true;
-            }
+
 
             if (!max)
-                this.currentPos.X++;
+                move = 1;
             else
-                this.currentPos.X--;
+                move = -1;
 
-            this.Transform3D.TranslateBy(new Vector3(this.currentPos.X , 0, 0));
+            if(this.dir == -1) //X
+            {
+                b = new Vector3((this.currentPos.X + move), 0, 0);
+            }
+            else if(this.dir == 1) //Y
+            {
+                b = new Vector3(0, (this.currentPos.Y + move), 0);
+            }
+            else //Z
+            {
+                b = new Vector3(0, 0, (this.currentPos.Z + move));
+            }
+
+            this.Transform3D.TranslateBy(b);
+            this.currentPos = b;
 
         }
 
         public void Deactivate()
         {
             System.Diagnostics.Debug.WriteLine("Moving Platform deactivate (doesn't work yet)");
-            //this.Transform3D.TranslateBy(this.starPos);
+            //this.Transform3D.TranslateBy(this.starPos);t
             //this.currentPos = this.StartPos;
             isActivated = false;
         }
