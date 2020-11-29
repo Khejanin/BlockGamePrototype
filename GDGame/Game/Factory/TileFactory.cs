189657e0d207e8496b24717ba2e9182e4ae6c117
Aleksandrs <axel1002@gmail.com>
2020-11-29 18:05:01 +0000

    Revert "Revert "Falling tile""
    This reverts commit 9ae59f51174798fe0cd9fa69ecb48a2ef5b3dd4b.

--- GDGame/Game/Factory/TileFactory.cs
+++ GDGame/Game/Factory/TileFactory.cs
@@ -37,6 +37,7 @@ namespace GDGame.Factory
                 ETileType.Enemy => CreateEnemy(),
                 ETileType.Button => CreateButton(),
                 ETileType.MovingPlatform => CreateMovingPlatform(),
+                ETileType.FallingPlatform => CreateFallingPlatform(),
                 ETileType.Spike => CreateSpike(),
                 ETileType.Star => CreatePickup(),
                 ETileType.Checkpoint => CreateCheckpoint(),
@@ -140,6 +141,15 @@ namespace GDGame.Factory
             if (platform?.ControllerList.Find(controller => controller.GetControllerType() == ControllerType.Movement) is TileMovementComponent tileMovementComponent) tileMovementComponent.Tile = platform;
             objectManager.Add(platform);
             return platform;
+        }
+
+        private BasicTile CreateFallingPlatform()
+        {
+            FallingTile fallingTile = (FallingTile)drawnActors["FallingTile"];
+            fallingTile = fallingTile.Clone() as FallingTile;
+            if (fallingTile?.ControllerList.Find(controller => controller.GetControllerType() == ControllerType.Movement) is TileMovementComponent tileMovementComponent) tileMovementComponent.Tile = fallingTile;
+            objectManager.Add(fallingTile);
+            return fallingTile;
         }
 
         private BasicTile CreateSpike()
